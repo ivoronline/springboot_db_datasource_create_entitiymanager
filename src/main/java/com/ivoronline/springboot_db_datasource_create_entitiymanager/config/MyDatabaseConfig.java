6508649da272bdaffda21c8799d1cae49d556a67
ivoronline <ivoronline@gmail.com>
2024-08-06 12:40:01 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_db_datasource_create_entitiymanager/config/MyDatabaseConfig.java
+++ src/main/java/com/ivoronline/springboot_db_datasource_create_entitiymanager/config/MyDatabaseConfig.java
@@ -1,13 +1,17 @@
 package com.ivoronline.springboot_db_datasource_create_entitiymanager.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories(
@@ -15,6 +19,8 @@ import javax.sql.DataSource;
   entityManagerFactoryRef = "myEntityManagerFactoryBean"
 )
 public class MyDatabaseConfig {
+
+	@Autowired private Environment env;
 
   //=========================================================================================================
   // DATA SOURCE PROPERTIES
@@ -41,10 +47,32 @@ public class MyDatabaseConfig {
     EntityManagerFactoryBuilder entityManagerFactoryBuilder,
     DataSource                  dataSource
   ) {
-    return entityManagerFactoryBuilder
-          .dataSource(dataSource)
-          .packages("com.ivoronline.springboot_db_datasource_create_entitiymanager.entity")
-          .build();
+  
+    //Hibernate Properties from application.properties are ignored => We need to set them manually
+    HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+                              hibernateJpaVendorAdapter.setGenerateDdl(true);
+                              hibernateJpaVendorAdapter.setShowSql(true);
+  
+    LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+      entityManagerFactory.setDataSource(dataSource);
+      entityManagerFactory.setPackagesToScan("com.ivoronline.springboot_db_datasource_create_entitiymanager.entity");
+      entityManagerFactory.setPersistenceUnitName("myunit");
+      entityManagerFactory.setJpaVendorAdapter(hibernateJpaVendorAdapter); //Properties don't work without it
+      entityManagerFactory.setJpaProperties(additionalProperties());
+      
+    return entityManagerFactory;
+    
   }
   
+   //=========================================================================================================
+  // ADDITIONAL PROPERTIES
+  //=========================================================================================================
+  // Reading Hibernate Properties from application.properties
+  private Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("spring.jpa.hibernate.ddl-auto"));
+		properties.setProperty("hibernate.show_sql"    , env.getProperty("spring.jpa.show-sql"          ));
+		return properties;
+	}
+	
 }
